a0946492c857045112afedf6914650c4758d39bd
Quentin <fayet.quentin@gmail.com>
2016-10-27 22:47:36 +0200

    Initialized lightController with GPIOs pin #

--- server/controller/Direction.py
+++ server/controller/Direction.py
@@ -4,6 +4,4 @@ from event import Event
 
 class Direction(Observer):
     def handle(self, event):
-        print('Received ', event.get_value())
-        response = Event('hello')
-        event.respond_to_emmiter(response)
+        pass
--- server/controller/LightController.py
+++ server/controller/LightController.py
@@ -1,8 +1,30 @@
 from observable import Observer
 from event import Event
+import RPi.GPIO as GPIO
+
 
 class LightController(Observer):
+
+    _gpios = None
+
+    def __init__(self):
+        self._gpios = {
+            'red': 11,
+            'green': 12,
+            'blue': 13,
+        }
+
     def handle(self, event):
-        print('Received ', event.get_value())
-        response = Event('light controller')
-        event.respond_to_emmiter(response)
+        event_value = event.get_value()
+
+        if event_value['component'] != 'light':
+            return
+
+        #TODO : Add 'command' existence checking
+
+        if event_value['command'] == 'setColor':
+            self.setColor(event_value)
+
+
+    def setColor(self, event_value):
+        pass
